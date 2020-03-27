d572bbe1a0ff24967659a7f688e3bb3389d282e7
animevn <haanhth21@gmail.com>
2020-03-27 14:30:15 +0700

    refract

--- src/Home.js
+++ src/Home.js
@@ -31,52 +31,56 @@ function Home() {
     setShow(true);
   }
 
+  const numberBox = (
+    <div style={{"height":"150px"}}
+         className="col-xl-5 col-lg-5 col-md-7 col-sm-9 col-9 mx-auto rounded
+                      d-flex align-items-center flex-column border border-success">
+      <p className="my-auto"
+         style={{
+           "font-size": `${size}rem`,
+           "visibility": `${show ? "visible" : "hidden"}`
+         }}>
+        {count}
+      </p>
+    </div>
+  );
+
+  const buttons = (name, action)=>{
+    return (
+      <button className="btn btn-outline-success" onClick={action}>
+        {name}
+      </button>
+    )
+  };
+
+  const buttonsBox = (children)=>{
+    return (
+      <div className="col-xl-5 col-lg-5 col-md-7 col-sm-9 col-9 mx-auto mt-5">
+        {children}
+      </div>
+    )
+  };
+
+  const rowBox = (children)=>{
+    return (
+      <div className="row d-flex justify-content-around mb-5">
+        {children}
+      </div>
+    )
+  };
+
+  const toggle = show ? "Hide" : "Show";
+
   return (
     <div className="container mt-5">
-
-      <div style={{"height":"150px"}}
-           className="col-xl-5 col-lg-5 col-md-7 col-sm-9 col-9 mx-auto rounded
-                      d-flex align-items-center flex-column border border-success">
-        <p className="my-auto"
-           style={{
-             "font-size": `${size}rem`,
-             "visibility": `${show ? "visible" : "hidden"}`
-           }}>
-          {count}
-        </p>
-      </div>
-
-      <div className="col-xl-5 col-lg-5 col-md-7 col-sm-9 col-9 mx-auto mt-5">
-
-        <div className="row d-flex justify-content-around mb-5">
-          <button className="btn btn-outline-success" onClick={onAddClick}>
-            Add
-          </button>
-          <button className="btn btn-outline-success" onClick={onTakeClick}>
-            Take
-          </button>
-        </div>
-
-        <div className="row d-flex justify-content-around mb-5">
-          <button className="btn btn-outline-success" onClick={onGrowClick}>
-            Grow
-          </button>
-          <button className="btn btn-outline-success" onClick={onShrinkClick}>
-            Shrink
-          </button>
-        </div>
-
-        <div className="row d-flex justify-content-around mb-5">
-          <button className="btn btn-outline-success" onClick={onShowClick}>
-            {show ? "Hide" : "Show"}
-          </button>
-          <button className="btn btn-outline-success" onClick={onResetClick}>
-            Reset
-          </button>
-        </div>
-
-      </div>
-
+      {numberBox}
+      {buttonsBox (
+        [
+          rowBox ([buttons("Add", onAddClick), buttons("Take", onTakeClick)]),
+          rowBox ([buttons("Grow", onGrowClick), buttons("Shrink", onShrinkClick)]),
+          rowBox ([buttons(toggle, onShowClick), buttons("Reset", onResetClick)])
+        ]
+      )}
     </div>
   )
 }
